f72d9a6e5e35766e735cc079013e1082b687d021
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-06-29 00:20:48 +0530

    Time: 3 ms (68.48%), Space: 5.9 MB (58.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,14 +3,16 @@ public:
    
     int fib(int n) { 
         // 1d dp
-       vector<int> dp(31,-1);
+         int prev1 = 1;
+         int prev2 = 0;
+        if(n == 0) return n;
   
-          dp[0]= 0;
-          dp[1]= 1;
-
           for(int i=2; i<=n; i++){
-              dp[i] = dp[i-1]+ dp[i-2];
+             int cur = prev1 + prev2;
+              
+              prev2 =prev1;
+              prev1 =cur;
           }
-          return dp[n];
+          return prev1;
     }
 };

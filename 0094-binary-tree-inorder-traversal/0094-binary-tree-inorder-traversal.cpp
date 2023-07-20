b2dff06289effdf1de26c5d64bf6ddbb709d1126
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-07-20 21:46:17 +0530

    Time: 3 ms (44.58%), Space: 8.2 MB (85.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,32 +11,51 @@
  */
 class Solution {
 private:   
-    // ITERATIVE 
-    void inorder(TreeNode* root, vector<int> &ans){
-        if(root == NULL) return;
-        
-        stack<TreeNode* > s;
-        
-        while(true){
-            if(root){
-                s.push (root);
-                root= root->left;
-            }
-            else{
-                if(s.empty()) break;
-                
-                root = s.top();
-                ans.push_back(root->val);
-                s.pop();
-                root = root->right;
-            }
-        } 
-        
-    }
+    
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        inorder(root, ans);
-        return ans;
+        vector<int>inorder;
+        TreeNode* cur = root;
+        while(cur != NULL)
+        {
+            // CASE - I 
+            // IF LEFT TREE IS EMPTY
+            if(cur-> left == NULL)
+            {
+                inorder.push_back(cur-> val);
+                cur = cur-> right;
+            }
+            // CASE - II 
+            // IT HAS A LEFT SUB TREE
+            else
+            {
+                // we dont wanna lose cur hence creating new one
+                TreeNode* prev = cur->left;
+                // traverse right untill it is either null or points to itself;
+                while(prev -> right && prev-> right != cur)
+                {
+                    prev = prev -> right;
+                }
+                // case I if right becomes null
+                if(prev -> right == NULL)
+                {
+                    prev -> right = cur;
+                    // now move cur to left
+                    cur  = cur-> left;
+                }
+                else
+                {
+                    // case II it is already pointing to someone
+                    // make it null
+                    prev-> right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur-> right;
+                }
+            }
+
+        }
+        // TC - O(N) + del(N) ~ O(N)
+        // SC - O(1)
+        return inorder;
     }
 };

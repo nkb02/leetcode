1627e8beb6f363a5a8f386c0cc4a7ba8d657790e
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-07-24 09:46:12 +0530

    Time: 51 ms (37.76%), Space: 10.7 MB (34.11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        // freq, char, ind
-        vector<pair<int, int>> hash(26,pair<int,int>({0,0}));
-        for(int i = 0; i <s.size(); i++){
-            hash[s[i]-'a'].first++;
-            hash[s[i]-'a'].second = i;
+        unordered_map<char, int> m;
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]]++;
         }
-        sort(hash.begin(), hash.end());
-        int i = 0;
-        while(i< 26 && hash[i].first != 1) i++;
-        if(i <26)
-            return hash[i].second;
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == 1) {
+                return i;
+            }
+        }
         return -1;
     }
 };

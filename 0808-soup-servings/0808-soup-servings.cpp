b627ee2e80a118e03f8ea8adfaf755e9769ffb4c
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-07-29 14:10:48 +0530

    Time: 48 ms (17.11%), Space: 391.1 MB (5.32%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -22,7 +22,7 @@ public:
         return dp[a][b] = 0.25*prob;
     }
     double soupServings(int n) {
-        if(n >= 8000 ) return 1.0;
+        if(n >= 10000 ) return 1.0;
         dp.resize(n+1, vector<double>(n+1, -1.0));
         
         return solve(n, n);

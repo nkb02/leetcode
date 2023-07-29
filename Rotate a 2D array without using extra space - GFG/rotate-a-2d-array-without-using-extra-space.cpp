b1d3f275ce661f7f01d13f69213c178aebae945a
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-07-29 14:51:58 +0530

    Added solution - LeetHub

---
+++ Rotate a 2D array without using extra space - GFG/rotate-a-2d-array-without-using-extra-space.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+private:
+    void transpose(vector<vector<int>>& arr, int n){
+        for(int i = 0; i< n; i++){
+            for(int j =i; j < n; j++)
+                swap(arr[i][j], arr[j][i]);
+        }
+        return;
+    }
+public:	
+	
+	void rotateMatrix(vector<vector<int>>& arr, int n) {
+	    // code here   
+	    for(int i = 0; i< n; i++){
+	        reverse(arr[i].begin(), arr[i].end());
+	    }
+	    
+	    transpose(arr, n);
+	    return;
+	    
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n;
+        vector<vector<int>> arr;
+        for (int i = 0; i < n; i++) {
+            vector<int> a;
+            for (int j = 0; j < n; j++) {
+                cin >> x;
+                a.push_back(x);
+            }
+            arr.push_back(a);
+        }
+        Solution ob;
+        ob.rotateMatrix(arr, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << arr[i][j] << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

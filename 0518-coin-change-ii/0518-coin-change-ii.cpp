8a1bdacab2bc103bd28fe8013d07b5f56f249e51
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-11 18:09:16 +0530

    Time: 38 ms (44.07%), Space: 29.9 MB (5.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,23 +2,29 @@ class Solution {
 public:
     int change(int T, vector<int>& arr) {
         int n =arr.size();
-        vector<vector<long>> dp(n+1, vector<long>(T+1, -1));
+        vector<vector<long>> dp(n+1, vector<long>(T+1, 0));
         
-        return solve(arr, n-1, T, dp );
+        // edge case
+        
+        for(int target = 0; target <= T; target++){
+            if(target % arr[0] == 0) 
+                dp[0][target] = 1;
+        }
+        for(int ind = 1; ind < n ; ind++){
+           
+            for(int target = 0; target <= T; target++){
+                long notTake = dp[ind-1][target];
+                long take = 0;
+                if(arr[ind] <= target){
+                    take = dp[ind][target-arr[ind]];
+                }
+
+                dp[ind][target] = take + notTake;
+
+            }
+        }
+        
+        return  dp[n-1][T];
     }
     
-    long solve(vector<int> &arr, int ind, int target, vector<vector<long>> &dp){
-        if(ind == 0){
-            return (target % arr[ind] == 0);
-        }
-        if(dp[ind][target] != -1) return dp[ind][target];
-        long notTake = solve(arr, ind-1, target, dp);
-        
-        long take = 0;
-        if(arr[ind] <= target){
-            take = solve(arr, ind, target-arr[ind], dp);
-        }
-        
-        return dp[ind][target] = take + notTake;
-    }
 };

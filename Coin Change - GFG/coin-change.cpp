6d9fbf7324d9cea890fe4e033c975472a6862c65
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-11 18:18:54 +0530

    Added solution - LeetHub

---
+++ Coin Change - GFG/coin-change.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long int count(int coins[], int N, int target) {
+
+        // unbounded knapsack
+        vector<vector<long long int> > dp(N+1, vector<long long int> (target +1, 0));
+        // edge case
+        for(int sum = 0 ; sum <= target; sum++){
+            if(sum % coins[0] == 0) dp[0][sum] = 1;
+        }
+        for(int ind = 1; ind < N; ind++){
+             for(int sum = 0 ; sum <= target; sum++){
+                long long take = 0, notTake;
+                if(coins[ind] <= sum){
+                    take = dp[ind][sum - coins[ind]];
+                }
+                // not take
+               
+                notTake =  dp[ind-1][sum];
+                
+                dp[ind][sum] = notTake + take;     
+            }
+        }
+        return dp[N-1][target];
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends

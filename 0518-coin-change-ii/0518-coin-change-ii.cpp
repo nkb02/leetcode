8d83545240640fc8f98621ecaeb5e57fea13761a
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-11 18:11:21 +0530

    Time: 26 ms (57.25%), Space: 7.7 MB (80.92%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int change(int T, vector<int>& arr) {
         int n =arr.size();
-        vector<vector<long>> dp(n+1, vector<long>(T+1, 0));
-        
+        // vector<vector<long>> dp(n+1, vector<long>(T+1, 0));
+        vector<long> prev(T+1, 0), cur(T+1, 0);
         // edge case
         
         for(int target = 0; target <= T; target++){
             if(target % arr[0] == 0) 
-                dp[0][target] = 1;
+                prev[target] = 1;
         }
         for(int ind = 1; ind < n ; ind++){
            
             for(int target = 0; target <= T; target++){
-                long notTake = dp[ind-1][target];
+                long notTake = prev[target];
                 long take = 0;
                 if(arr[ind] <= target){
-                    take = dp[ind][target-arr[ind]];
+                    take = cur[target-arr[ind]];
                 }
 
-                dp[ind][target] = take + notTake;
+                cur[target] = take + notTake;
 
             }
+            prev = cur;
         }
         
-        return  dp[n-1][T];
+        return  prev[T];
     }
     
 };

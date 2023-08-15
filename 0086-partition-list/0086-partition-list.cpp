eb3952a64d1672715f5bcf276752d123071cd340
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-15 16:28:16 +0530

    Time: 6 ms (50.56%), Space: 10.4 MB (21.22%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,25 +11,26 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode before(0), after(0);
-        ListNode* before_curr = &before;
-        ListNode* after_curr = &after;
-        
-        while(head) {
-            if(head->val < x) {
-                before_curr->next = head;
-                before_curr = head;
-            } else {
-                after_curr->next = head;
-                after_curr = head;
+        ListNode* temp=head;
+        ListNode* dummy=new ListNode (-1);
+        ListNode *ans=dummy;
+        while(temp!=NULL){
+            if(temp->val<x){
+                dummy->next=new ListNode(temp->val);
+                dummy=dummy->next;
             }
-            head = head->next;
+            temp=temp->next;
         }
-        
-        after_curr->next = nullptr;
-        before_curr->next = after.next;
-        
-        return before.next;
-        
+        temp=head;
+        while(temp!=NULL){
+        if(temp->val>=x){
+        dummy->next=new ListNode(temp->val);
+        dummy=dummy->next;
+        }
+        temp=temp->next;
+
+             }
+         return ans->next;
+
     }
 };

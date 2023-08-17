a9b77cbcb5c7d5e0a9f382f86723a59b729b216a
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-17 14:44:02 +0530

    Time: 77 ms (72.10%), Space: 34.3 MB (42.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        if (mat.empty() || mat[0].empty())
-            return {};
-
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> queue;
-        int MAX_VALUE = m * n;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.push({i, j});
-                } else {
-                    mat[i][j] = MAX_VALUE;
-                }
-            }
-        }
-        
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        while (!queue.empty()) {
-            auto [row, col] = queue.front(); queue.pop();
-            for (auto [dr, dc] : directions) {
-                int r = row + dr, c = col + dc;
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[row][col] + 1) {
-                    queue.push({r, c});
-                    mat[r][c] = mat[row][col] + 1;
-                }
-            }
-        }
-        
-        return mat;
+         int n = mat.size();
+	    int m = mat[0].size();
+	    vector<vector<int>> vis(n, vector<int>(m, 0));
+	    vector<vector<int>> dist(n, vector<int>(m, 0));
+	    queue<pair<pair<int, int>, int>> q;
+	    // nxm
+	    for(int  i =0 ; i<n ; i++)
+	    {
+	        for(int  j =0 ; j<m ; j++){
+	            if(mat[i][j] == 0){
+	                q.push({{i,j},0});
+	                vis[i][j] = 1;
+	            }
+	        }
+	    }
+	    int drow[] = {-1,0,1,0};
+	    int dcol[] = {0,1,0,-1};
+	    while(!q.empty()){
+	        int row = q.front().first.first;
+	        int col = q.front().first.second;
+	        int step = q.front().second;
+	        
+	        q.pop();
+	        dist[row][col] = step;
+	        for(int i = 0; i<4 ; i++){
+	            int nrow = row + drow[i];
+	            int ncol = col + dcol[i];
+	            if(nrow >= 0 && nrow<n && ncol >= 0 && ncol < m 
+	            && vis[nrow][ncol] == 0 )
+	            {
+	                vis[nrow][ncol] = 1;
+	                q.push({{nrow,ncol} ,step+1});
+	            }
+	        }
+	    }
+	    
+	    return dist;
         
     }
 };

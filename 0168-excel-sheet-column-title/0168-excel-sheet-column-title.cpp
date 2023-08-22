a90496802e06dd817617bab56abb9f0deef49609
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-22 16:03:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    string convertToTitle(int n) {
-        return n == 0 ? "" : convertToTitle((n - 1) / 26) + (char) ((n - 1) % 26 + 'A');
+    string convertToTitle(int columnNumber) {
+        
+        string ans ="";
+        while(columnNumber > 0){
+            ans.push_back((columnNumber-1)%26 + 'A');
+            columnNumber = (columnNumber-1)/26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 

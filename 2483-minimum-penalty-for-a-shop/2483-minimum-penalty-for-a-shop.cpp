903f6de917b429a7f0b348d25f499e6ebdafe6d8
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-08-29 11:51:09 +0530

    Time: 37 ms (39.38%), Space: 15 MB (40.41%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -4,19 +4,19 @@ public:
         int n = customers.size();
         vector<int> dp(n+1, 0);
         for(auto it : customers) if(it == 'N') dp[n]++;
+        
+        int pos = n;
+        int mini = dp[n];
+ 
         for(int i = n-1; i >= 0; i--){
             dp[i] = dp[i+1] + (customers[i] == 'Y') - (customers[i] == 'N');
-        }
-        
-        int pos = -1;
-        int mini = INT_MAX;
-        
-        for(int i = 0; i<= n; i++){
-            if(dp[i] < mini){
+             if(dp[i] <= mini){
                 mini = dp[i];
                 pos = i;
             }
         }
+        
+       
         return pos;
     }
 };

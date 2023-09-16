684d2f9114a831a851c5be8c6c40cfc457e9869c
Nitish Kumar <93542952+nkb02@users.noreply.github.com>
2023-09-17 00:55:38 +0530

    Time: 9 ms (37.50%), Space: 23.2 MB (37.50%) - LeetHub

--- 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.cpp
+++ 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.cpp
@@ -11,19 +11,15 @@ public:
                 pos = i;
             }
         }
-        vector<int> temp = nums;
         int  x = pos;
         int cnt = 1, i = 0;
+        
         while(cnt < n){
             if(nums[(x+i)%n] > nums[(x+i+1) % n]) return -1;
             i++;
             cnt++;
         }
-        // sort(temp.begin(), temp.end());
-        // for(int i = 0; i < n;i ++){
-        //     if(temp[i] != nums[(x + i) % n])
-        //         return -1;
-        // }
+     
         if(pos == 0)
             return pos;
         else
